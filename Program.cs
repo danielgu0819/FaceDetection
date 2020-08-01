d52b15e431f779b1917ee230e8ed68e1f7cc3ffb
danielgu0819 <danielgu0819@gmail.com>
2020-08-01 14:06:37 -0600

    0801 update program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace test_c3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! 0801");
         }
     }
 }
