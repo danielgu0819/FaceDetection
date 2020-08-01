3ab50b8769c4e4f76d94029503fbd0cd75468b33
danielgu <danielgu0819@gmail.com>
2020-08-01 00:00:33 -0600

    0731 add test_c3

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace test_c3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
